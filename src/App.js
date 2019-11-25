c706a62199d6654e55d026b57a58428f96e7a179
Wade Stewart <stew.wade@gmail.com>
2019-11-24 21:47:42 -0500

    rendering all elements on page for mobile, need bigger base image and to add functionality part two lol

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
 
 import baseLayer from './img/base.png';
 
@@ -7,16 +8,16 @@ function App() {
   return (
     <div className="App">
       <div className="container center-align">
-        <div>
+        <div className="title">
           <h4 className="center-align">Generate a Random Character</h4>
         </div>
         <div className="base-image">
           <img src={baseLayer} alt="base layer" />
         </div>
-        <div>
+        <div className="button">
           <a className="waves-effect waves-light btn"><i className="material-icons right">shuffle</i>Randomize</a>
         </div>
-        <div>
+        <div className="button">
           <a className="waves-effect waves-light btn"><i className="material-icons right">get_app</i>PNG</a>
         </div>
       </div>
